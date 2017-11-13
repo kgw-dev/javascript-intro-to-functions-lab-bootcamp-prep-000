0c16f9acc3fcd82dda3f7aaf939c75e8a99ffbb4
kent winter <kgw700@gmail.com>
2017-11-13 01:56:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log (string.toLowerCase())
 }
-function sayHiToGrandma("hello") {
+function sayHiToGrandma(string) {
   return string
 }
+var string="hello"
